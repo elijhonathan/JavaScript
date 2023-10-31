e5a9c6668f537f05d3ede446ca02bbf980016814
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-31 20:59:47 -0300

    Add files via upload

---
+++ Abobra.js
@@ -0,0 +1,14 @@
+let nomePokemon = "Pikachu"
+let nivelPokemon = 20
+let pontosDeVidaPokemon = 45
+let sexoPokemon = "M"
+let pokemonAtivo = true
+
+console.log("O seu Pokemon " + nomePokemon + ", com o nível " + nivelPokemon + ", com " + pontosDeVidaPokemon + " de pontos de vida, está ativo - " + pokemonAtivo)
+
+
+
+
+
+
+
