789a4dbd3c0a8e0411b82f920c55ef2ccf9af2b5
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-31 20:32:31 -0300

    Add files via upload

---
+++ Abobra.js
@@ -0,0 +1,11 @@
+
+const notificacao = "Pokemon Go diz: "
+
+
+console.log(notificacao + "tem um novo pokemon na sua região!")
+
+console.log( notificacao + "você foi derrotado por um líder!")
+
+
+
+
