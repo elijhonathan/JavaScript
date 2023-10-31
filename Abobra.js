05fb3159ed0a6b0c65f6805c823f47f8c9e7230a
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-31 20:43:46 -0300

    Add files via upload

---
+++ Abobra.js
@@ -0,0 +1,21 @@
+let poteCafe = "Café Pilão"
+
+let poteAcucar = "Açucar Cristal"
+
+let poteBiscoito = "Biscoito Maizena"
+
+const cozinhaVovo = "Hoje na cozinha da vovó tem: "
+
+console.log(cozinhaVovo + poteCafe + ", " + poteAcucar + " e " + poteBiscoito)
+
+poteCafe = "Café 3 Corações"
+
+poteAcucar = "Açucar União"
+
+poteBiscoito = "Biscoito Cream Cracker"
+
+console.log(cozinhaVovo + poteCafe + ", " + poteAcucar + " e " + poteBiscoito)
+
+
+
+
