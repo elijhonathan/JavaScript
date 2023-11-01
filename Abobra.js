2115740c79649b7c94a212d8fb2151fcc8e15976
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-11-01 16:23:29 -0300

    Add files via upload

---
+++ Abobra.js
@@ -0,0 +1,14 @@
+let pokemon = ["Pikachu", "Charmander", "Bulbassaur", "Squirt"]
+
+let timePokemon = [
+    ["Pikachu", "M", "Level 1"],
+    ["Charmander", "F", "Level 3"],
+    ["Bulbasaur", "M", "Level 2"],
+    ["Squirt", "F", "Level 4"]
+]
+
+console.log(pokemon[3])
+console.log(timePokemon[2])
+console.log(timePokemon[3][2])
+
+console.log("O pokemon " + pokemon[1] + " está no level " + timePokemon[1][2])
