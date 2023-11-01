ffacf20218c12ad14c1f6ffbf8967f1541ed3d6a
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-31 21:16:18 -0300

    Add files via upload

---
+++ Abobra.js
@@ -0,0 +1,16 @@
+let nome = "Jurubeba da silva"
+let idade = 69
+let numeroTelefone = "(41) 99965-5522"
+let cpf = "458.695.368-55"
+let endereco = "Avenida Iguaçu da Cangibra"
+let recebePensao = true
+
+console.log("A " + nome + " com " + idade + " de idade, com o cpf " + cpf + " com o telefone " + numeroTelefone + " que reside no endereço "
++ endereco + " Recebe pensão - " + recebePensao  )
+
+
+
+
+
+
+
