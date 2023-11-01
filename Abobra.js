b13ecad0b5836b79454af2a1552d13bdda2abb91
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-11-01 18:11:23 -0300

    Add files via upload

---
+++ Abobra.js
@@ -0,0 +1,58 @@
+// operadores Matemáticos:
+
+// Soma +
+let valorConta = 15.50
+let coverArtistico = 3
+let valorTotal = valorConta + coverArtistico
+console.log("O Total da conta é " + valorTotal)
+
+// Subtração -
+let salario = 1500.90
+let vale = 375.55
+let valorAReceber = salario - vale
+console.log("O salario a receber será: " + valorAReceber)
+
+// Multiplicação *
+let contribuicaoPorPessoa = 30
+let numeroPessoas = 12
+let valorArrecadado = contribuicaoPorPessoa * numeroPessoas
+console.log("O valor total arrecado foi: " + valorArrecadado)
+
+// Divisão /
+let valorCompra = 1802
+let quantidadePessoas = 36
+let valorPorPessoa = valorCompra / quantidadePessoas
+console.log("O valor por pessoa será " + valorPorPessoa)
+
+// Resto Divisão %
+let restanteDoValorPorPessoa = valorCompra % quantidadePessoas
+console.log("O valor da compra de  " + valorCompra + " dividido pelo numero de pessoas que é " + quantidadePessoas + " tem como resto da divisão " + restanteDoValorPorPessoa)
+
+// Incremento ++
+let idCadastro = 1
+console.log("O Id de cadastro é " + idCadastro)
+console.log("Atualização: Novo Cadastro Realizado!")
+idCadastro++
+console.log("O Id de cadastro é " + idCadastro)
+
+// Decremento --
+let quantidadeDePessoasNaFila = 35
+console.log("A quantidade de pessoas na fila é: " + quantidadeDePessoasNaFila)
+console.log("Atualização: Uma pessoa foi atendida")
+quantidadeDePessoasNaFila--
+console.log("A quantidade de pessoas na fila é: " + quantidadeDePessoasNaFila)
+
+// Atribuição
+// variavel += valor
+// variavel -= valor
+// variavel *= valor
+// variavel /= valor
+// variavel %= valor
+
+// Escopos 
+// Resultado 20
+let resultado = 2 * 5 + 5
+console.log(resultado)
+let resultadocerto = 2 * (5 + 5)
+console.log(resultadocerto)
+
