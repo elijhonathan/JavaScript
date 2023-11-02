1655e2f1f6c24962107006f2d4b34b99f183f168
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-11-02 12:22:07 -0300

    Add files via upload

---
+++ Abobra.js
@@ -0,0 +1,27 @@
+// Operadores de Comparação
+// = é atribuição
+// == é comparação
+// === é comparação de conteúdo e formato
+
+let numero = "1"
+//console.log(numero == "1")
+resultado = numero == "1"
+console.log(resultado)
+
+console.log(numero == 1)
+console.log(numero === 1)
+
+// !=
+let marca = "Apple"
+console.log( marca != "Xiaomi")
+console.log(numero !== 1)
+
+// >, <, >= e <=
+let comparar = 800
+console.log(comparar > 800)
+console.log(comparar < 800)
+console.log(comparar >= 800)
+console.log(comparar <= 801)
+
+
+
