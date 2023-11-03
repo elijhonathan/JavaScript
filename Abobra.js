897c1c9aa49f70663550cd2f8ebbce4e4ab2a156
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-11-03 18:09:38 -0300

    Add files via upload

---
+++ Abobra.js
@@ -0,0 +1,18 @@
+let ehLigado = true
+
+if(ehLigado)
+{
+    console.log("Executou!!!")
+}
+
+let possuiOvos = true
+
+let itensComprados = ""
+
+if(possuiOvos)
+{
+    itensComprados = "Leite, ovos"
+}
+
+console.log("Itens comprados: " + itensComprados)
+
