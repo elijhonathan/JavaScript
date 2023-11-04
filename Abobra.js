d505446ada28e6b450cd2da29793e2c11d42a197
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-11-04 18:17:51 -0300

    Add files via upload

---
+++ Abobra.js
@@ -0,0 +1,14 @@
+
+let resultado = soma(5, 5)
+let resultado2 = soma(1584, 3658)
+
+function soma(numA, numB)
+{
+    //let somatorio = numA + numB
+    //return somatorio
+    return numA + numB
+}
+
+console.log(`O resultado da função deu ${resultado}`)
+console.log(`O resultado da segunda função deu ${resultado2}`)
+console.log("O resultado deu " + soma(6, 785))
