37d416b154fc3f5b5bcb2ec9aefb88257e698514
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-11-04 17:18:00 -0300

    Add files via upload

---
+++ Abobra.js
@@ -0,0 +1,17 @@
+
+torrar()
+injetarPao()
+torrar()
+injetarPao()
+
+function torrar()
+{
+    console.log("torrando pão")
+}
+
+
+
+function injetarPao()
+{
+    console.log("preparando para injetar o pão")
+}
