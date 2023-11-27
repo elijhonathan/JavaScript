7db2ea91c891a8592e74b51fa542c7d44d86402f
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-11-27 20:43:58 -0300

    Update dioTech01-GastoCombustivelKm.js

--- dioTech01-GastoCombustivelKm.js
+++ dioTech01-GastoCombustivelKm.js
@@ -9,3 +9,10 @@ Você terá 3 variaveis. Sendo elas:
 Imprima no console o valor que será gasto de combustivel para realizar a viagem.
 */
 
+var precoCombustivel = 6.00;
+var gastoMedioLitroPorKm = 15;
+var distanciaViagem = 300;
+
+var valorTotalCombustivel = (distanciaViagem / gastoMedioLitroPorKm) * precoCombustivel
+
+console.log(`O valor total gasto para fazer está viagem será R$ ${valorTotalCombustivel}`)
