be0fc711d4c95435dfd75dc1a767b6021a1f95fa
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-11-27 20:33:44 -0300

    Add files via upload

---
+++ Abobra.js
@@ -0,0 +1,11 @@
+/*
+Faça um programa para calcular o valor de uma viagem
+
+Você terá 3 variaveis. Sendo elas:
+1 - Preço combustivel;
+2 - Gasto médio de combustivel do carro por km;
+3 - Distancia em km da viagem
+
+Imprima no console o valor que será gasto de combustivel para realizar a viagem.
+*/
+
