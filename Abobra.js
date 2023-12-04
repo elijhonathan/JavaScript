11de86ded62afd4576458ac7fda8a1b1a1091376
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-12-04 18:04:28 -0300

    Add files via upload

---
+++ Abobra.js
@@ -0,0 +1,12 @@
+/*
+Crie um programa que dado um numero faça a sua tabuada de 1 a 10:
+*/
+
+const numero = 6;
+
+
+
+for (let i = 1; i < 11; i++)
+{
+    console.log(`${numero} x ${i} = ${numero * i}`);
+}
